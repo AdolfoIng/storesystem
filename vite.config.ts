811bbf98dde60308dce067385d583f17e1dc1573
Adolfo Copa <adolfo.dev.soft@gmail.com>
2025-04-27 19:47:14 -0400

    Cambios en vite.config, para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/storesystem/',
+  base: 'https://adolfoing.github.io/storesystem/',
 })
